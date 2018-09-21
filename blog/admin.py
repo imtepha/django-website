7217507acbd976d100fc3c955a93a81683d5839d
Stephanie <imtephabone@gmail.com>
2018-09-21 20:13:10 +0800

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+#absolue import importing from outside current app
+from .models import Post 
+#explicit relative import use when import from other module in current app
+
+admin.site.register(Post)
