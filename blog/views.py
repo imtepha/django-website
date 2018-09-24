bd496566ea382e3c2d8e002db759b7ce71cc5625
Stephanie <imtephabone@gmail.com>
2018-09-24 20:30:36 +0800

    Modified template

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 #def render(request, template_name, context=None, content_type=None, status=None, using=None)
 # Create your views here.
+# ouerysets doc https://docs.djangoproject.com/en/2.0/ref/models/querysets/
 def post_list(request):
-    return render(request,'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    return render(request,'blog/post_list.html', {'posts':posts}) #the curly braclet will be use in the html
